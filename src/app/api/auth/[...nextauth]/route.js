c99325579831816af50e07cabf97e67744a94a40
Serginbr7 <henriquesilvasergio10@gmail.com>
2026-01-09 22:45:06 -0300

    test a new login door

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,15 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 
 const handler = NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-})
+  // debug em caso de erro
+  debug: true,
+});
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
