7af5ae34bf5b12c0ccb8c299158031729cd674f8
Serginbr7 <henriquesilvasergio10@gmail.com>
2026-01-14 21:10:21 -0300

    final login correction

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,24 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import VercelPostgresAdapter from "@/lib/auth-adapter"; // Importa o arquivo da pasta lib
 
 const handler = NextAuth({
+  adapter: VercelPostgresAdapter(), // <--- AQUI ESTÁ A MÁGICA: Conecta ao banco
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  // Adiciona segredo para criptografar os tokens (importante em produção)
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id; // Garante que o ID do banco vá para o frontend
+      }
+      return session;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
